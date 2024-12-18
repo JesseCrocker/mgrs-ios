be9f0008653f2286f0ad4668900dfcd486f62ac5
Jesse Crocker <jesse@datamongers.net>
2024-12-18 14:54:08 -0700

    package.swift file

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "mgrs-ios",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "mgrs_ios",
+            targets: ["mgrs_ios"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/jessecrocker/grid-ios.git", .branch("swift-package")),
+    ],
+    targets: [
+        .target(
+            name: "mgrs_ios",
+            dependencies: ["grid_ios"],
+            path: "mgrs-ios",
+            resources: [
+                .process("mgrs.plist")
+            ]
+        ),
+        .testTarget(
+            name: "mgrs-iosTests",
+            dependencies: ["mgrs_ios"],
+            path: "mgrs-iosTests"
+        )
+    ]
+)
